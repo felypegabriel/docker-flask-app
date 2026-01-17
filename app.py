76748ca5f69b871ce9fff258131f056b8ad7ac15
felypegabriel <felypegabriel@unitins.br>
2026-01-17 11:22:56 -0300

    Refactor Flask app route and run configuration
    Replaced the home function with hello_world and updated the run configuration.

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 from flask import Flask
 
-
 app = Flask(__name__)
 
+@app.route('/')
+def hello_world():
+    return 'Aplicação Python com Flask rodando no Docker!'
 
-@app.route("/")
-def home():
-return "Aplicação Flask rodando com Docker com sucesso!"
-
-
-if __name__ == "__main__":
-app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    # host='0.0.0.0' 
+    app.run(debug=True, host='0.0.0.0', port=5000)
